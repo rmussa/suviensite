b4815fadc3b39f248ad5b22fd6bf414788d797cc
rmussa <raheem.mussa@gmail.com>
2014-12-30 17:41:43 -0500

    committing html, css, js. testing this

---
+++ js/angular_suvien.js
@@ -0,0 +1,19 @@
+function sFeatures($scope) {
+    $scope.features = [
+			              {
+			            	  title:"Quick & Easy Setup",
+							  description:"Sign up easily with social media. Import the photos and videos that you want.",
+							  src: "img/icons/icon_upload.png"
+			              },
+			              {
+			            	  title:"Simple to Use",
+							  description:"Upload content and let Suvien do the rest. It's that simple.",
+							  src: "img/icons/icon_check.png"
+			              },
+			              {
+			            	  title:"Shared Respect",
+							  description:"You don't have to share your location, details or anything to anyone on the platform.",
+							  src: "img/icons/icon_love.png"
+			              }
+			            ];
+}
---
+++ js/suvien.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){  
+ //    $("div.carousel3").css("margin-left",-$(this).width());
+	
+	// $('#myCarousel2').on('appear', function() {
+ //      // this element is now inside browser viewport
+ //      // $('carousel3').animate({marginLeft:0}, 1000);
+ //      alert('appear');
+ //    });
+
+	$( #myCarousel2 ).scroll(function() {
+	  alert('appear');
+	});
+});
+
