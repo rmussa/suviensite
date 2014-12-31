7f3526827615c1feceb15b6c0fcfe632aa6a1690
rmussa <raheem.mussa@gmail.com>
2014-12-30 19:03:30 -0500

    Update angular_suvien.js
    changed image types

--- js/angular_suvien.js
+++ js/angular_suvien.js
@@ -3,17 +3,17 @@ function sFeatures($scope) {
 			              {
 			            	  title:"Quick & Easy Setup",
 							  description:"Sign up easily with social media. Import the photos and videos that you want.",
-							  src: "img/icons/icon_upload.png"
+							  src: "img/icons/icon_upload_gray.png"
 			              },
 			              {
 			            	  title:"Simple to Use",
 							  description:"Upload content and let Suvien do the rest. It's that simple.",
-							  src: "img/icons/icon_check.png"
+							  src: "img/icons/icon_check_gray.png"
 			              },
 			              {
 			            	  title:"Shared Respect",
 							  description:"You don't have to share your location, details or anything to anyone on the platform.",
-							  src: "img/icons/icon_love.png"
+							  src: "img/icons/icon_love_gray.png"
 			              }
 			            ];
 }
